8cacbdb5067d2bc7d87960b5165775ba0440dd6e
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-11-01 06:42:25 +0000

    [Finish]いいねの非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,32 +1,28 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    if request.referer.include?("books/#{book.id}")
-      redirect_to book_path(book)
-    elsif request.referer.include?("users")
-      redirect_to user_path(current_user.id)
-    else
-      redirect_to books_path
-    end
 
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    if request.referer.include?("books/#{book.id}")
-      redirect_to book_path(book)
-    elsif request.referer.include?("users")
-      redirect_to user_path(current_user.id)
-    else
-      redirect_to books_path
-    end
 
   end
 end
+
+
+#
+#if request.referer.include?("books/#{book.id}")
+#  redirect_to book_path(book)
+#elsif request.referer.include?("users")
+#  redirect_to user_path(current_user.id)
+#else
+#  redirect_to books_path
+#end
 
 #簡略化バージョン
 #class FavoritesController < ApplicationController
