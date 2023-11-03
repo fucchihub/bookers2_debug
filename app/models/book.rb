7153438db1459daf5e676f1cbacafc9d384e119b
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-11-03 15:38:58 +0000

    [Finish]応用課題7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,14 @@ class Book < ApplicationRecord
   validates :title, presence:true
   validates :body, presence:true, length:{maximum:200}
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+                                                  #6日前の始まり(00:00:00)から今日の終わり(23:59:59)まで
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+                                                  #2週間前の始まり(00:00:00)から1週間前の終わり(23:59:59)まで
+
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
