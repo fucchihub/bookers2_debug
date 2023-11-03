66eca686abe4a78f140b8de0fd3b23f5dbf73e45
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-11-03 18:00:17 +0000

    [Finish]応用課題8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -15,6 +15,11 @@ class Book < ApplicationRecord
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
                                                   #2週間前の始まり(00:00:00)から1週間前の終わり(23:59:59)まで
 
+  scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
+  #以下追加
+  def self.past_week_count
+   (1..6).map { |n| created_days_ago(n).count }.reverse
+  end
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
