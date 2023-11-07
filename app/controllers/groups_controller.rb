104fc35d8cb7b17793c7c2b2ca795bf607076de6
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-11-07 01:36:21 +0000

    [Finish]応用課題9c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,6 @@ class GroupsController < ApplicationController
   def show
     @book = Book.new
     @group = Group.find(params[:id])
-    @user = User.find(params[:id])
   end
 
   def new
@@ -39,6 +38,18 @@ class GroupsController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def new_mail
+    @group = Group.find(params[:group_id])
+  end
+
+  def send_mail
+    @group = Group.find(params[:group_id])
+    group_users = @group.users
+    @mail_title = params[:mail_title]
+    @mail_content = params[:mail_content]
+    ContactMailer.send_mail(@mail_title, @mail_content, group_users, @group).deliver
   end
 
   private
