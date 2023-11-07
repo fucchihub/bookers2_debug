986e5662c5f58ff783b7ed02768b9dc760ebd3c0
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-11-07 17:50:51 +0000

    [Finish]応用課題8d

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,10 @@ class Book < ApplicationRecord
   validates :title, presence:true
   validates :body, presence:true, length:{maximum:200}
 
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  scope :star_count, -> {order(star: :desc)}
+
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
